c0c30f73d00b1a522a1b3bf745c231b16bfc0f61
TechPioneerX <juanmejiawww@outlook.com>
2024-04-02 16:12:28 +0100

    updated text encoding

--- main.py
+++ main.py
@@ -47,11 +47,19 @@ else:
     @echo off
     echo =========================================
     echo Hello from Dynamic Batch Script
+echo. 8888888b.                   888                                 8888888b.          d8b          888    
+echo. 888   Y88b                  888                                 888   Y88b         Y8P          888    
+echo. 888    888                  888                                 888    888                      888    
+echo. 888   d88P .d88b.  .d8888b  888888 .d88b.  888d888 .d88b.       888   d88P .d88b.  888 88888b.  888888 
+echo. 8888888P" d8P  Y8b 88K      888   d88""88b 888P"  d8P  Y8b      8888888P" d88""88b 888 888 "88b 888    
+echo. 888 T88b  88888888 "Y8888b. 888   888  888 888    88888888      888       888  888 888 888  888 888    
+echo. 888  T88b Y8b.          X88 Y88b. Y88..88P 888    Y8b.          888       Y88..88P 888 888  888 Y88b.  
+echo. 888   T88b "Y8888   88888P'  "Y888 "Y88P"  888     "Y8888       888        "Y88P"  888 888  888  "Y888 
     echo =========================================
     """
 
     # Create a temporary file in the system's temporary directory
-    with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix=".bat") as temp_file:
+    with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix=".bat", encoding='utf-8') as temp_file:
         temp_file.write(batch_script)
         temp_file_path = temp_file.name
 
